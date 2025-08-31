564abae7ef4e1e88e9f60be64cac632577efc070
Dmitriy <cubarrv@gmail.com>
2025-08-31 13:16:33 +0300

    test_1

--- answers/tests.py
+++ answers/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Answer
+from questions.models import Question
 
-# Create your tests here.
+class AnswersTestCase(APITestCase):
+
+    def setUp(self):
+        self.question = Question.objects.create(text="test question")
+        self.answer = Answer.objects.create(text="test answer", question_id=self.question)
+
+    def test_create_answer(self):
+        url = f'/questions/{self.question.id}/answers/'
+        data = {'text': 'test answer_2'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Answer.objects.count(), 2)
+        self.assertEqual(Answer.objects.filter(text='test answer_2').count(), 1)
+
+    def test_get_answer(self):
+        url = f'/answers/{self.answer.id}/'  
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['text'], 'test answer')
+
+    def test_delete_answer(self):
+        url = f'/answers/{self.answer.id}/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Answer.objects.count(), 0)
--- questions/tests.py
+++ questions/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Question
 
-# Create your tests here.
+class QuestionTestCase(APITestCase):
+
+    def setUp(self):
+        self.question = Question.objects.create(text="test question")
+
+    def test_create_question(self):
+        url = '/questions/'
+        data = {'text': 'test question_2'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Question.objects.count(), 2)
+        self.assertEqual(Question.objects.filter(text='test question_2').count(), 1)
+
+    def test_get_questions(self):
+        url = '/questions/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['text'], 'test question')
+
+    def test_get_question(self):
+        url = f'/questions/{self.question.id}/'  
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['text'], 'test question')
+
+    def test_delete_question(self):
+        url = f'/questions/{self.question.id}/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Question.objects.count(), 0)
+        
+        
+
+
